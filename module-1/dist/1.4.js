390df2a244b4d0d22796f74db95f5f506bfdc1b4
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2024-05-06 09:43:24 +0600

    object and literel types

---
+++ module-1/dist/1.4.js
@@ -0,0 +1,22 @@
+"use strict";
+// basic data type
+// string
+let firstName = "mohiuddin";
+console.log(firstName);
+// number
+let roll = 1234;
+console.log(roll);
+// boolen
+let isAdmin = true;
+console.log(isAdmin);
+// undefined
+let x = undefined;
+// null
+let y = null;
+// any
+let d;
+d = 1234;
+// array
+let friends = ["mohsin", 'sojib',];
+// tuple
+let cooordinate = [3244, "mjgjjhj"];
---
+++ module-1/src/1.5.ts
@@ -0,0 +1,14 @@
+// object ====> referance type
+
+const userName: {
+    company: "Programming Hero"; //literel types
+    firstName: string;
+    middleName?: string; //optional type
+    lastName: string;
+    isMarried: boolean;
+} = {
+    company: "Programming Hero",
+    firstName: "mohi",
+    lastName: "uddin",
+    isMarried: false
+}
